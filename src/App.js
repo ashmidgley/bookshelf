84f2670a0106ebdd4fe663563dd546f60cc9d1da
Ash <Ashley.Midgley@intergen.co.nz>
2019-01-07 14:07:21 +1300

    Refactor of home page into component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/header/header';
+import Home from './components/home/home';
 import Review from './components/review/review';
-import { Heading, Card, Media, Content } from 'react-bulma-components';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './App.css';
 
@@ -43,24 +43,7 @@ class App extends Component {
           <Header />
 
           <Route exact={true} path="/" render={() => (
-            <div>
-              {this.reviews.map(review =>
-                <Card style={{ "maxWidth" : "300px", "margin" : "50px auto" }}>
-                <Link to={`/review/${review.id}`}>
-                    <Card.Image src={review.image} />
-                </Link>
-                <Card.Content>
-                    <Media>
-                    <Media.Item>
-                        <Heading size={4}>{review.title}</Heading>
-                        <Heading subtitle size={6}>{review.createdOn}</Heading>
-                    </Media.Item>
-                    </Media>
-                    <Content>{review.content.substr(0, 50)}...</Content>
-                </Card.Content>
-                </Card>
-                )}
-            </div>
+            <Home reviews={this.reviews} />
           )} />
 
           <Route path="/review/:reviewId" render={({ match }) => (
