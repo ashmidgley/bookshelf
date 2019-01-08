aec84e7d84717d1ed2c1ff2e54631e5fd3ab014e
Ashley Midgley <AshleyM@intergen.org.nz>
2019-01-09 12:07:51 +1300

    Addition of event listener to adjust desktop column sizing.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,15 +6,43 @@ import './home.css'
 
 class Home extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            columnSize: null
+        }
+    }
+
     componentDidMount() {
         window.scrollTo(0, 0);
+        this.checkDimensions();
+        window.addEventListener("resize", this.checkDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.checkDimensions);
+    }
+
+    /* = () => binds 'this' automatically to checkDimensions */
+    checkDimensions = () => {
+        var newVal = null;
+        if(window.innerWidth < 769) {
+            newVal = null;
+        } else if(window.innerWidth > 769 && window.innerWidth < 900) {
+            newVal = 'half';
+        } else if(window.innerWidth > 900 && window.innerWidth < 1200) {
+            newVal = 'one-third';
+        } else if(window.innerWidth > 1000) {
+            newVal = 'one-quarter';
+        }
+        this.setState({columnSize: newVal}); 
     }
 
     render(){
         return (
             <Columns id="parent">
                 {this.props.reviews.map(review =>
-                    <Columns.Column key={review.id} size="one-quarter" className="child">
+                    <Columns.Column key={review.id} size={this.state.columnSize} className="child">
                         <Link to={`/review/${review.id}`}>
                             <Card>
                             <Card.Image src={window.location.origin + '/images/' + review.image} />
