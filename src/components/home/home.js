b73b8bb9067b2b7e3b98869d0de500233505bd18
Ashley Midgley <AshleyM@intergen.org.nz>
2019-01-09 11:21:29 +1300

    Refactoring home page to use css media queries rather than react-media.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Heading, Card, Media as Med, Content, Columns } from 'react-bulma-components';
 import { Link } from 'react-router-dom';
-import Media from 'react-media';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './home.css'
 
@@ -13,55 +12,27 @@ class Home extends Component {
 
     render(){
         return (
-            <Media query="(max-width: 600px)">
-                {matches => matches ? 
-                    (
-                        <Columns style={{ "margin" : "auto 15px" }}>
-                            {this.props.reviews.map(review =>
-                                <Columns.Column key={review.id} style={{ "marginTop" : "30px", "padding" : "1.25rem" }}>
-                                    <Link to={`/review/${review.id}`}>
-                                        <Card>
-                                        <Card.Image src={window.location.origin + '/images/' + review.image} />
-                                        <Card.Content>
-                                            <Med>
-                                            <Med.Item>
-                                                <Heading size={6}>{review.title}</Heading>
-                                                <Heading subtitle size={6}>By {review.author}</Heading>
-                                            </Med.Item>
-                                            </Med>
-                                            <Content>{review.content.substr(0, 50)}...</Content>
-                                            <Heading size={6}>{review.createdOn}</Heading>
-                                        </Card.Content>
-                                        </Card>
-                                    </Link>
-                                </Columns.Column>
-                            )}
-                        </Columns>
-                    ) : (
-                        <Columns style={{ "margin" : "auto 80px" }}>
-                            {this.props.reviews.map(review =>
-                                <Columns.Column key={review.id} size="one-quarter" style={{ "marginTop" : "30px", "padding" : "1.25rem" }}>
-                                    <Link to={`/review/${review.id}`}>
-                                        <Card>
-                                        <Card.Image src={window.location.origin + '/images/' + review.image} />
-                                        <Card.Content>
-                                            <Med>
-                                            <Med.Item>
-                                                <Heading size={6}>{review.title}</Heading>
-                                                <Heading subtitle size={6}>By {review.author}</Heading>
-                                            </Med.Item>
-                                            </Med>
-                                            <Content>{review.content.substr(0, 50)}...</Content>
-                                            <Heading size={6}>{review.createdOn}</Heading>
-                                        </Card.Content>
-                                        </Card>
-                                    </Link>
-                                </Columns.Column>
-                            )}
-                        </Columns>
-                    )
-                }
-            </Media>
+            <Columns id="parent">
+                {this.props.reviews.map(review =>
+                    <Columns.Column key={review.id} size="one-quarter" className="child">
+                        <Link to={`/review/${review.id}`}>
+                            <Card>
+                            <Card.Image src={window.location.origin + '/images/' + review.image} />
+                            <Card.Content>
+                                <Med>
+                                <Med.Item>
+                                    <Heading size={6}>{review.title}</Heading>
+                                    <Heading subtitle size={6}>By {review.author}</Heading>
+                                </Med.Item>
+                                </Med>
+                                <Content>{review.content.substr(0, 50)}...</Content>
+                                <Heading size={6}>{review.createdOn}</Heading>
+                            </Card.Content>
+                            </Card>
+                        </Link>
+                    </Columns.Column>
+                )}
+            </Columns>
         )
     }
 }
