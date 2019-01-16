1e23ded70669c9b4195c7d011b43dc205d2def3a
Ashley Midgley <AshleyM@intergen.org.nz>
2019-01-17 10:16:41 +1300

    Refactor of review page.

--- src/components/review/review.js
+++ src/components/review/review.js
@@ -11,13 +11,13 @@ class Review extends Component {
     render() {
         return (
             <div className="review-body">
-                {/* <div className="review-header">
-                    <Heading size={2}>{this.props.review.title}</Heading>
-                    <Heading subtitle>By {this.props.review.author}</Heading>
+                <div className="review-header">
+                    <h1 className="title is-2">{this.props.review.title}</h1>
+                    <h1 className="subtitle">By {this.props.review.author}</h1>
                     <img src={'/images/' + this.props.review.image} alt='Review Book' />
                 </div>
                 <div className="review-content" dangerouslySetInnerHTML={{ __html: this.props.review.content }}></div>
-                <Heading size={6} id="createdon">{this.props.review.createdOn}</Heading> */}
+                <p id="createdon">{this.props.review.createdOn}</p>
                 <Link to={'/'}>Back to Home</Link>
             </div>
         );
