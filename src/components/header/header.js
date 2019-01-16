225801a76809b03b4c7e92128cd25edce2e3ed6d
Ashley Midgley <AshleyM@intergen.org.nz>
2019-01-16 14:22:55 +1300

    Clean up plus testing of horizontal images in review.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,14 +6,17 @@ import './header.css';
 
 class Header extends Component {
 
+  title = "2019 Reads";
+  subtitle = "Man vs. New Year's resolution.";
+
   render() {
     return (
         <Section id="header-body">
             <Hero color="info">
             <Hero.Body id="header-hero">
                 <Container id="header-container">
-                    <Heading size={1}><Link to={'/'}>Midgley's 2019 Reads</Link></Heading>
-                    <Heading subtitle>Ticking off the New Year's resolution one cover at a time.</Heading>
+                    <Heading size={1}><Link to={'/'}>{this.title}</Link></Heading>
+                    <Heading subtitle>{this.subtitle}</Heading>
                 </Container>
             </Hero.Body>
             </Hero>
