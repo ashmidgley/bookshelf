1027e65f6fb6fb6b51913b5572c54fc804f3b3c6
Ash <Ashley.Midgley@intergen.co.nz>
2019-04-12 16:23:34 +1200

    REMOVING IIS CONFIGURATION.

--- iis/copy.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-const webConfigPath = './build/web.config';
-
-if (fs.existsSync(webConfigPath)) {
-    fs.unlinkSync(webConfigPath);
-}
-
-fs.copySync('./iis/web.config', webConfigPath);
