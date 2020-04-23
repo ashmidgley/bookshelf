4aecbd0928029307ab989d32b330cea74ec47964
Ash Midgley <ayelgdom@tutanota.com>
2020-04-23 16:01:50 +0100

    Add loading image.

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -6,11 +6,7 @@ class Loading extends React.Component {
     render() {
         return (
             <div className="spinner">
-                <div className="rect1"></div>
-                <div className="rect2"></div>
-                <div className="rect3"></div>
-                <div className="rect4"></div>
-                <div className="rect5"></div>
+               <img src="/logo.png"/>
             </div> 
         )
     }
