2e836d8bd29d3177a31115357623e9925464389f
Ash Midgley <ayelgdom@tutanota.com>
2020-04-25 15:56:37 +0100

    Get loading spinner back.

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -6,7 +6,11 @@ class Loading extends React.Component {
     render() {
         return (
             <div className="spinner">
-               <img src="/logo.png"/>
+                <div className="rect1"></div>
+                <div className="rect2"></div>
+                <div className="rect3"></div>
+                <div className="rect4"></div>
+                <div className="rect5"></div>
             </div> 
         )
     }
