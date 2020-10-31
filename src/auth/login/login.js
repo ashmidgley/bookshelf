e3a17dd0be40befaac98d75140e9ddf6bbad83ee
ash.midgley <ash.midgley@thecomplianceworkbook.com>
2020-10-31 16:29:54 +1300

    Refactor rest of auth to hooks.

--- src/auth/login/login.js
+++ src/auth/login/login.js
@@ -11,192 +11,172 @@ import {
 } from "../../shared/field-validator";
 import { login } from "../auth.service";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submitting: false,
-      error: null,
-    };
+const Login = ({ history }) => {
+  const [submitting, setSubmitting] = React.useState(false);
+  const [error, setError] = React.useState();
 
-    this.handleSuccess = this.handleSuccess.bind(this);
-    this.handleError = this.handleError.bind(this);
-  }
-
-  submitEntry(values) {
-    this.setState({
-      error: null,
-      submitting: true,
-    });
+  const submitEntry = (values) => {
+    setError(null);
+    setSubmitting(true);
 
     var user = {
       email: values.email,
       password: values.password,
     };
 
-    this.login(user);
-  }
+    loginUser(user);
+  };
 
-  login(user) {
+  const loginUser = (user) => {
     login(user)
       .then((response) => {
-        this.handleSuccess(response);
+        handleSuccess(response);
       })
       .catch((error) => {
-        this.handleError(error);
+        handleError(error);
       });
-  }
+  };
 
-  handleSuccess(userId) {
-    this.setState({
-      submitting: false,
-    });
-    this.props.history.push(`/shelf/${userId}`);
-  }
+  const handleSuccess = (userId) => {
+    setSubmitting(false);
+    history.push(`/shelf/${userId}`);
+  };
 
-  handleError(error) {
-    this.setState({
-      error: error,
-      submitting: false,
-    });
-  }
+  const handleError = (error) => {
+    setError(error);
+    setSubmitting(false);
+  };
 
-  render() {
-    return (
-      <section className="section hero is-fullheight mobile-auth">
-        <Helmet>
-          <title>Login - Bookshelf</title>
-          <meta
-            name="description"
-            content="Log into Bookshelf to start keeping track of the books you've been reading."
-          />
-        </Helmet>
-        <div className="hero-body mobile-auth">
-          <div className="container">
-            <div className="columns is-centered">
-              <div className="column is-two-fifths">
-                <div className="card header-background">
-                  <header className="card-header">
-                    <p className="card-header-title">
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faLock} size="sm" />
-                      </span>
-                      <span>Login</span>
-                    </p>
-                  </header>
-                  <div className="card-content">
-                    <Formik
-                      initialValues={{
-                        email: "",
-                        password: "",
-                      }}
-                      validate={(values) => {
-                        let errors = {};
-                        if (!validateEmail(values.email))
-                          errors.email = "Incorrect email format";
-                        if (!values.email) errors.email = "Required";
-                        if (!validatePasswordLength(values.password))
-                          errors.password =
-                            "Password must be at least 5 characters long";
-                        if (!values.password) errors.password = "Required";
-                        return errors;
-                      }}
-                      onSubmit={(values, { setSubmitting }) => {
-                        this.submitEntry(values);
-                        setSubmitting(false);
-                      }}
-                    >
-                      {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-                      }) => (
-                        <form className="form" onSubmit={handleSubmit}>
-                          <div className="field">
-                            <label className="label custom-label">Email</label>
-                            <div className="control is-clearfix">
-                              <input
-                                autoFocus="autofocus"
-                                className={
-                                  errors.email && touched.email
-                                    ? "input is-danger"
-                                    : "input"
-                                }
-                                type="text"
-                                name="email"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.email}
-                              />
-                            </div>
+  return (
+    <section className="section hero is-fullheight mobile-auth">
+      <Helmet>
+        <title>Login - Bookshelf</title>
+        <meta
+          name="description"
+          content="Log into Bookshelf to start keeping track of the books you've been reading."
+        />
+      </Helmet>
+      <div className="hero-body mobile-auth">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-two-fifths">
+              <div className="card header-background">
+                <header className="card-header">
+                  <p className="card-header-title">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faLock} size="sm" />
+                    </span>
+                    <span>Login</span>
+                  </p>
+                </header>
+                <div className="card-content">
+                  <Formik
+                    initialValues={{
+                      email: "",
+                      password: "",
+                    }}
+                    validate={(values) => {
+                      let errors = {};
+                      if (!validateEmail(values.email))
+                        errors.email = "Incorrect email format";
+                      if (!values.email) errors.email = "Required";
+                      if (!validatePasswordLength(values.password))
+                        errors.password =
+                          "Password must be at least 5 characters long";
+                      if (!values.password) errors.password = "Required";
+                      return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      submitEntry(values);
+                      setSubmitting(false);
+                    }}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                    }) => (
+                      <form className="form" onSubmit={handleSubmit}>
+                        <div className="field">
+                          <label className="label custom-label">Email</label>
+                          <div className="control is-clearfix">
+                            <input
+                              autoFocus="autofocus"
+                              className={
+                                errors.email && touched.email
+                                  ? "input is-danger"
+                                  : "input"
+                              }
+                              type="text"
+                              name="email"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.email}
+                            />
                           </div>
-                          <div id="password-field" className="field">
-                            <label className="label custom-label">
-                              Password
-                            </label>
-                            <div className="control is-clearfix">
-                              <input
-                                className={
-                                  errors.password && touched.password
-                                    ? "input is-danger"
-                                    : "input"
-                                }
-                                type="password"
-                                name="password"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.password}
-                              />
-                            </div>
+                        </div>
+                        <div id="password-field" className="field">
+                          <label className="label custom-label">Password</label>
+                          <div className="control is-clearfix">
+                            <input
+                              className={
+                                errors.password && touched.password
+                                  ? "input is-danger"
+                                  : "input"
+                              }
+                              type="password"
+                              name="password"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.password}
+                            />
                           </div>
-                          {this.state.error && (
-                            <div className="notification is-danger custom-notification">
-                              {this.state.error}
-                            </div>
-                          )}
-                          <div id="forgot-password">
-                            <Link to="/forgot-password">
-                              Forgotten password?
+                        </div>
+                        {error && (
+                          <div className="notification is-danger custom-notification">
+                            {error}
+                          </div>
+                        )}
+                        <div id="forgot-password">
+                          <Link to="/forgot-password">Forgotten password?</Link>
+                        </div>
+                        <hr />
+                        <div className="field is-grouped">
+                          <div className="control">
+                            <button
+                              type="submit"
+                              disabled={isSubmitting}
+                              className={
+                                submitting
+                                  ? "button is-dark is-loading"
+                                  : "button is-dark"
+                              }
+                            >
+                              Login
+                            </button>
+                          </div>
+                          <div className="control">
+                            <Link to="/" className="button is-outlined">
+                              Cancel
                             </Link>
                           </div>
-                          <hr />
-                          <div className="field is-grouped">
-                            <div className="control">
-                              <button
-                                type="submit"
-                                disabled={isSubmitting}
-                                className={
-                                  this.state.submitting
-                                    ? "button is-dark is-loading"
-                                    : "button is-dark"
-                                }
-                              >
-                                Login
-                              </button>
-                            </div>
-                            <div className="control">
-                              <Link to="/" className="button is-outlined">
-                                Cancel
-                              </Link>
-                            </div>
-                          </div>
-                        </form>
-                      )}
-                    </Formik>
-                  </div>
+                        </div>
+                      </form>
+                    )}
+                  </Formik>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 Login.propTypes = {
   history: PropTypes.shape({
--- src/auth/register/register.js
+++ src/auth/register/register.js
@@ -11,197 +11,179 @@ import {
 } from "../../shared/field-validator";
 import { register } from "../auth.service";
 
-class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submitting: false,
-      error: null,
-    };
+const Register = ({ history }) => {
+  const [submitting, setSubmitting] = React.useState(false);
+  const [error, setError] = React.useState();
 
-    this.handleSuccess = this.handleSuccess.bind(this);
-    this.handleError = this.handleError.bind(this);
-  }
-
-  submitEntry(values) {
-    this.setState({
-      error: null,
-      submitting: true,
-    });
+  const submitEntry = (values) => {
+    setError(null);
+    setSubmitting(true);
 
     var user = {
       email: values.email,
       password: values.password,
     };
 
-    this.register(user);
-  }
+    registerUser(user);
+  };
 
-  register(user) {
+  const registerUser = (user) => {
     register(user)
       .then((response) => {
-        this.handleSuccess(response);
+        handleSuccess(response);
       })
       .catch((error) => {
-        this.handleError(error);
+        handleError(error);
       });
-  }
+  };
 
-  handleSuccess(userId) {
-    this.setState({
-      submitting: false,
-    });
-    this.props.history.push(`/shelf/${userId}`);
-  }
+  const handleSuccess = (userId) => {
+    setSubmitting(false);
+    history.push(`/shelf/${userId}`);
+  };
 
-  handleError(error) {
-    this.setState({
-      error: error,
-      submitting: false,
-    });
-  }
+  const handleError = (error) => {
+    setError(error);
+    setSubmitting(false);
+  };
 
-  render() {
-    return (
-      <section className="section hero is-fullheight mobile-auth">
-        <Helmet>
-          <title>Register - Bookshelf</title>
-          <meta
-            name="description"
-            content="Join the Bookshelf community for free! Add books to your shelf. Rate them. Put them into categories. Share with friends."
-          />
-        </Helmet>
-        <div className="hero-body mobile-auth">
-          <div className="container">
-            <div className="columns is-centered">
-              <div className="column is-two-fifths">
-                <div className="card header-background">
-                  <header className="card-header">
-                    <p className="card-header-title">
-                      <span className="icon">
-                        <FontAwesomeIcon icon={faLock} size="sm" />
-                      </span>
-                      <span>Register</span>
-                    </p>
-                  </header>
-                  <div className="card-content">
-                    <Formik
-                      initialValues={{
-                        email: "",
-                        password: "",
-                      }}
-                      validate={(values) => {
-                        let errors = {};
-                        if (!validateEmail(values.email))
-                          errors.email = "Incorrect email format";
-                        if (!values.email) errors.email = "Required";
-                        if (!validatePasswordLength(values.password))
-                          errors.password =
-                            "Password must be at least 5 characters long";
-                        if (!values.password) errors.password = "Required";
-                        return errors;
-                      }}
-                      onSubmit={(values, { setSubmitting }) => {
-                        this.submitEntry(values);
-                        setSubmitting(false);
-                      }}
-                    >
-                      {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-                      }) => (
-                        <form className="form" onSubmit={handleSubmit}>
-                          <div className="field">
-                            <label className="label custom-label">Email</label>
-                            <div className="control is-clearfix">
-                              <input
-                                autoFocus="autofocus"
-                                className={
-                                  errors.email && touched.email
-                                    ? "input is-danger"
-                                    : "input"
-                                }
-                                type="text"
-                                name="email"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.email}
-                              />
-                            </div>
-                            {errors.email && touched.email && (
-                              <div className="has-text-danger is-size-7 custom-validation">
-                                {errors.email}
-                              </div>
-                            )}
+  return (
+    <section className="section hero is-fullheight mobile-auth">
+      <Helmet>
+        <title>Register - Bookshelf</title>
+        <meta
+          name="description"
+          content="Join the Bookshelf community for free! Add books to your shelf. Rate them. Put them into categories. Share with friends."
+        />
+      </Helmet>
+      <div className="hero-body mobile-auth">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-two-fifths">
+              <div className="card header-background">
+                <header className="card-header">
+                  <p className="card-header-title">
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faLock} size="sm" />
+                    </span>
+                    <span>Register</span>
+                  </p>
+                </header>
+                <div className="card-content">
+                  <Formik
+                    initialValues={{
+                      email: "",
+                      password: "",
+                    }}
+                    validate={(values) => {
+                      let errors = {};
+                      if (!validateEmail(values.email))
+                        errors.email = "Incorrect email format";
+                      if (!values.email) errors.email = "Required";
+                      if (!validatePasswordLength(values.password))
+                        errors.password =
+                          "Password must be at least 5 characters long";
+                      if (!values.password) errors.password = "Required";
+                      return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      submitEntry(values);
+                      setSubmitting(false);
+                    }}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                    }) => (
+                      <form className="form" onSubmit={handleSubmit}>
+                        <div className="field">
+                          <label className="label custom-label">Email</label>
+                          <div className="control is-clearfix">
+                            <input
+                              autoFocus="autofocus"
+                              className={
+                                errors.email && touched.email
+                                  ? "input is-danger"
+                                  : "input"
+                              }
+                              type="text"
+                              name="email"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.email}
+                            />
                           </div>
-                          <div id="password-field" className="field">
-                            <label className="label custom-label">
-                              Password
-                            </label>
-                            <div className="control is-clearfix">
-                              <input
-                                className={
-                                  errors.password && touched.password
-                                    ? "input is-danger"
-                                    : "input"
-                                }
-                                type="password"
-                                name="password"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                value={values.password}
-                              />
-                            </div>
-                            {errors.password && touched.password && (
-                              <div className="has-text-danger is-size-7 custom-validation">
-                                {errors.password}
-                              </div>
-                            )}
-                          </div>
-                          {this.state.error && (
-                            <div className="notification is-danger custom-notification">
-                              {this.state.error}
+                          {errors.email && touched.email && (
+                            <div className="has-text-danger is-size-7 custom-validation">
+                              {errors.email}
                             </div>
                           )}
-                          <hr />
-                          <div className="field is-grouped">
-                            <div className="control">
-                              <button
-                                type="submit"
-                                disabled={isSubmitting}
-                                className={
-                                  this.state.submitting
-                                    ? "button is-dark is-loading"
-                                    : "button is-dark"
-                                }
-                              >
-                                Register
-                              </button>
-                            </div>
-                            <div className="control">
-                              <Link to="/" className="button is-outlined">
-                                Cancel
-                              </Link>
-                            </div>
+                        </div>
+                        <div id="password-field" className="field">
+                          <label className="label custom-label">Password</label>
+                          <div className="control is-clearfix">
+                            <input
+                              className={
+                                errors.password && touched.password
+                                  ? "input is-danger"
+                                  : "input"
+                              }
+                              type="password"
+                              name="password"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.password}
+                            />
                           </div>
-                        </form>
-                      )}
-                    </Formik>
-                  </div>
+                          {errors.password && touched.password && (
+                            <div className="has-text-danger is-size-7 custom-validation">
+                              {errors.password}
+                            </div>
+                          )}
+                        </div>
+                        {error && (
+                          <div className="notification is-danger custom-notification">
+                            {error}
+                          </div>
+                        )}
+                        <hr />
+                        <div className="field is-grouped">
+                          <div className="control">
+                            <button
+                              type="submit"
+                              disabled={isSubmitting}
+                              className={
+                                submitting
+                                  ? "button is-dark is-loading"
+                                  : "button is-dark"
+                              }
+                            >
+                              Register
+                            </button>
+                          </div>
+                          <div className="control">
+                            <Link to="/" className="button is-outlined">
+                              Cancel
+                            </Link>
+                          </div>
+                        </div>
+                      </form>
+                    )}
+                  </Formik>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 
 Register.propTypes = {
   history: PropTypes.shape({
